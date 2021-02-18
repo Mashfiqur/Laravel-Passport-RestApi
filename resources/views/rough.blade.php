4a0e166930d4cb9d7bea9ae54cb4a89887519e2a
Mashfiq-Office-Desktop <mashfiqurrr@gmail.com>
2021-02-18 16:16:45 +0600

    code upload for the first time with Passport package and two models

---
+++ resources/views/rough.blade.php
@@ -0,0 +1,11 @@
+Tutorial Link: https://www.positronx.io/laravel-rest-api-with-passport-authentication-tutorial/
+
+
+Personal access client created successfully.
+Client ID: 1
+Client secret: NxEB6HN3l8x647zN5IaOrh72FnT30QK58PTzLSPe
+Password grant client created successfully.
+Client ID: 2
+Client secret: WbtQah7oLCsly9cRJZvHimDEPp6B8rHNo53UuILS
+
+
